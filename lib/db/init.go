9ba4a9c59d575cabef541da474e1afc53bb470ef
Valentin Perraudin <valentinperraudin@gmail.com>
2017-10-24 19:15:27 +0200

    refac db, add KeepOpen (each 10min)

--- lib/db/init.go
+++ lib/db/init.go
@@ -4,7 +4,13 @@ import (
 	"../config/utils"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"sync"
+	"time"
+	"../log"
+	"os"
 )
+
+var mutex = &sync.Mutex{}
 
 func Init(api_c *utils.API) error {
 
@@ -20,4 +26,33 @@ func Init(api_c *utils.API) error {
 	}
 
 	return nil
+}
+
+func KeepOpen(api_c * utils.API) {
+	var err error
+
+	for {
+		time.Sleep(10 * time.Minute)
+
+		mutex.Lock()
+
+		if err = api_c.Database.Close(); err != nil {
+			log.FatalError("KeepOpen Close:", err.Error())
+			os.Exit(0)
+		}
+
+		if api_c.Database, err = sql.Open("mysql", api_c.Database_Info.DSN()); err != nil {
+			log.FatalError("KeepOpen Open:", err.Error())
+			os.Exit(0)
+		}
+
+		if err = api_c.Database.Ping(); err != nil {
+			api_c.Database.Close()
+			log.FatalError("KeepOpen Ping:", err.Error())
+			os.Exit(0)
+		}
+
+		mutex.Unlock()
+	}
+
 }
--- lib/db/request.go
+++ lib/db/request.go
@@ -6,14 +6,60 @@ import (
 	"database/sql"
 	"encoding/json"
 	"time"
+	"errors"
 )
+
+
+func LoadConfig(api_c *utils.API) ([]byte, error) {
+
+	var conf []byte
+
+	mutex.Lock()
+
+	if err := api_c.Database.QueryRow("SELECT configuration FROM configuration").Scan(&conf); err != nil {
+		return []byte{}, errors.New("LoadConfig : " + err.Error())
+	}
+
+	mutex.Unlock()
+
+	return conf, nil
+}
+
+func LoadSymbolStatus(api_c *utils.API, symbols *[]tools.Symbol) error {
+	var err error
+	var rows *sql.Rows
+
+	mutex.Lock()
+
+	if rows, err = api_c.Database.Query("SELECT id, reference, description, lot_max_size, lot_min_size, state FROM symbols"); err != nil {
+		return errors.New("LoadSymbolStatus Query : " + err.Error())
+	}
+
+	for rows.Next() {
+		var id int
+		var reference, description, state string
+		var lot_max_size, lot_min_size float64
+
+		if err = rows.Scan(&id, &reference, &description, &lot_max_size, &lot_min_size, &state); err != nil {
+			return errors.New("LoadSymbolStatus Scan : " + err.Error())
+		}
+
+		*symbols = append(*symbols, tools.Symbol{id, reference, description, state, lot_max_size, lot_min_size})
+	}
+
+	mutex.Unlock()
+
+	return nil
+}
 
 func LoadLastBidsForSymbol(api_c *utils.API, symbol tools.Symbol, bids *[]tools.Bid) error {
 	var err error
 	var rows *sql.Rows
 
+	mutex.Lock()
+
 	if rows, err = api_c.Database.Query("SELECT id, bid_at, last_bid, calculations FROM stock_values WHERE bid_at >= ? AND symbol_id = ?", api_c.From, symbol.Id); err != nil {
-		return err
+		return errors.New("LoadLastBidsForSymbol Query : " + err.Error())
 	}
 
 	for rows.Next() {
@@ -23,7 +69,7 @@ func LoadLastBidsForSymbol(api_c *utils.API, symbol tools.Symbol, bids *[]tools.
 		var calculations []byte
 
 		if err = rows.Scan(&id, &bid_at, &last_bid, &calculations); err != nil {
-			return err
+			return errors.New("LoadLastBidsForSymbol Scan : " + err.Error())
 		}
 
 		symbol.Description = ""
@@ -31,28 +77,7 @@ func LoadLastBidsForSymbol(api_c *utils.API, symbol tools.Symbol, bids *[]tools.
 		*bids = append(*bids, tools.Bid{id, symbol.Name, symbol, bid_at, time.Time{}, last_bid, string(calculations), map[string]float64{}})
 	}
 
-	return nil
-}
-
-func LoadSymbolStatus(api_c *utils.API, symbols *[]tools.Symbol) error {
-	var err error
-	var rows *sql.Rows
-
-	if rows, err = api_c.Database.Query("SELECT id, reference, description, lot_max_size, lot_min_size, state FROM symbols"); err != nil {
-		return err
-	}
-
-	for rows.Next() {
-		var id int
-		var reference, description, state string
-		var lot_max_size, lot_min_size float64
-
-		if err = rows.Scan(&id, &reference, &description, &lot_max_size, &lot_min_size, &state); err != nil {
-			return err
-		}
-
-		*symbols = append(*symbols, tools.Symbol{id, reference, description, state, lot_max_size, lot_min_size})
-	}
+	mutex.Unlock()
 
 	return nil
 }
@@ -60,17 +85,15 @@ func LoadSymbolStatus(api_c *utils.API, symbols *[]tools.Symbol) error {
 func InsertOrUpdateBid(api_c *utils.API, bid *tools.Bid) error {
 
 	var err error
-	var stmt *sql.Stmt
 
-	if stmt, err = api_c.Database.Prepare("INSERT INTO stock_values (`symbol_id`, `bid_at`, `last_bid`, `calculations`) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE `last_bid`= ?, `calculations` = ?"); err != nil {
-		return err
-	}
+	mutex.Lock()
 
 	by, _ := json.Marshal(bid.Calculations)
 
-	if _, err = stmt.Exec(bid.Symbol.Id, bid.Bid_at, bid.Last_bid, string(by), bid.Last_bid, string(by)); err != nil {
-		return err
+	if _, err = api_c.Database.Exec("INSERT INTO stock_values (`symbol_id`, `bid_at`, `last_bid`, `calculations`) VALUES (?, ?, ?, ?) ON DUPLICATE KEY UPDATE `last_bid`= ?, `calculations` = ?", bid.Symbol.Id, bid.Bid_at, bid.Last_bid, string(by), bid.Last_bid, string(by)); err != nil {
+		return errors.New("InsertOrUpdateBid Prepare : " + err.Error())
 	}
+	mutex.Unlock()
 
 	return nil
 }
